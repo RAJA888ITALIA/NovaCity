a6e609b7eebbdb1c18d04e3f34c43031ac2523fd
RAJA888ITALIA <rajazainfaisal888@gmail.com>
2026-06-06 13:49:53 +0200

    Update config.php

--- private/config.php
+++ private/config.php
@@ -1,13 +1,13 @@
 
 <?php
 // config.php
-// Gmail SMTP credentials
+// Gmail SMTP credentials [nascosto]
 define('EMAIL_USERNAME', getenv('EMAIL_USERNAME'));
 define('EMAIL_PASSWORD', getenv('EMAIL_PASSWORD'));
 define('EMAIL_VERIFY', getenv('EMAIL_VERIFY'));
 define('EMAIL_HOST', getenv('EMAIL_HOST'));
 
-//ServerHost
+//ServerHost [nascosto]
 define('DB_HOST', getenv('DB_HOST'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASS', getenv('DB_PASS'));
